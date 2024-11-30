b8622e68d97e3ccef38846d94571ced097477a30
Crouford <croufordz@gmail.com>
2024-11-30 21:02:46 +0500

    RLE_encode.js

--- RLE_encode.js
+++ RLE_encode.js
@@ -1,34 +1,39 @@
-function escape_encode(input){
+function code_compression(input) {
     let result = '';
     let i = 0;
-    
-    while (i<input.length) {
-        let currentSimbol = input[i];
-        let length = 1;
-        
-        while (currentSimbol == input[length+i]){
-            length++;
-        }
-    
-        if (length<=2){
-            result += currentSimbol.repeat(length);
+
+    while (i < input.length) {
+        let currentSym = input[i];
+        let length_same_sym = 1;
+
+        // Подсчет длины последовательности одинаковых символов
+        while (currentSym == input[length_same_sym+i]) {
+            length_same_sym++;
         }
 
-        else{
-            result += length + currentSimbol;
+        if (length_same_sym <= 2) {
+            result += currentSym.repeat(length_same_sym); // Оставляем символы как есть
+        } else {
+            result += length_same_sym + currentSym; // Добавляем количество и символ
         }
-        i += length;
-        
+
+        i += length_same_sym; // Переходим к следующей группе символов
     }
+
     return result;
 }
 
-let fs = require('fs'); //подключили библиотеку
+// Подключение библиотеки 
+let fs = require('fs');
 
-var inText = fs.readFileSync('input.txt'); //считали из файла
-var a = inText.toString() //сохранили в переменную как строку
+// Читаем входной файл
+var inText = fs.readFileSync('input.txt', 'utf-8'); // Убедитесь, что файл существует
+var a = inText.toString(); // Преобразуем в строку
 
-encode_str = escape_encode(a);
-fs.writeFileSync('code.txt', encode_str);
+// Сжимание строку 
+let encode_str = code_compression(a);
 
-console.log("Коэффициент сжатия = ", a.length/encode_str.length); // вывели степень сжатия
+// Записываем в файл
+fs.writeFileSync('out_code.txt', encode_str);
+
+console.log("Коэффициент сжатия = ", (a.length / encode_str.length)); // вывод кэфа
