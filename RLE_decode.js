14d9b5b6b97445546b77f9c68130f42bcb3b6321
Crouford <croufordz@gmail.com>
2024-11-30 21:00:36 +0500

    RLE_decode.js

--- RLE_decode.js
+++ RLE_decode.js
@@ -1,34 +1,31 @@
-function escape_decode(input){
+let fs = require('fs'); 
+
+function decompression(input) {
     let result = '';
     let i = 0;
-    let numbers = "1234567890";
+    let numbers = "0123456789";
 
-    while (i<input.length) {
+    while (i < input.length) {
         var count = '';
-        while(numbers.indexOf(input[i])!=-1){
-            count+=input[i];
+        while (numbers.indexOf(input[i]) != -1) {
+            count += input[i];
             i++;
         }
-        if(count.length>0){
-            count = Number(count)
-            result += input[i].repeat(count-1);
-        }
-        else{
-            result += input[i]
+        if (count.length > 0) {
+            count = Number(count);
+            result += input[i].repeat(count - 1);
+        } else {
+            result += input[i];
             i++;
         }
-
-        
-        
     }
 
     return result;
 }
 
-let fs = require('fs'); //подключили библиотеку
-var inCode = fs.readFileSync('code.txt');
-var b = inCode.toString()
-decode_str = escape_decode(b);
-fs.writeFileSync('decode.txt', decode_str);
+var file = fs.readFileSync('out_code.txt');
+var str_file = file.toString();
+decode_str = decompression(str_file);
+fs.writeFileSync('out_decode.txt', decode_str);
 
-console.log("Коэффициент сжатия = ", decode_str.length/b.length);
+console.log("Коэффициент сжатия = ", decode_str.length / str_file.length);
